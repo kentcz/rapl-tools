a9ac2bdc040aaf290148ebff8aeb006f8655f451
kentcz on zoka <kentcz@hotmail.com>
2015-03-24 23:00:20 -0400

    fix formating

--- Rapl.cpp
+++ Rapl.cpp
@@ -11,7 +11,7 @@
 
 #include "Rapl.h"
 
-#define MSR_RAPL_POWER_UNIT             0x606
+#define MSR_RAPL_POWER_UNIT            0x606
 
 /*
  * Platform specific RAPL Domains.
@@ -19,41 +19,41 @@
  * And DRAM RAPL Domain is supported on 062D only
  */
 /* Package RAPL Domain */
-#define MSR_PKG_RAPL_POWER_LIMIT				0x610
-#define MSR_PKG_ENERGY_STATUS					0x611
-#define MSR_PKG_PERF_STATUS						0x13
-#define MSR_PKG_POWER_INFO                      0x614
+#define MSR_PKG_RAPL_POWER_LIMIT       0x610
+#define MSR_PKG_ENERGY_STATUS          0x611
+#define MSR_PKG_PERF_STATUS            0x13
+#define MSR_PKG_POWER_INFO             0x614
 
 /* PP0 RAPL Domain */
-#define MSR_PP0_POWER_LIMIT                     0x638
-#define MSR_PP0_ENERGY_STATUS					0x639
-#define MSR_PP0_POLICY                          0x63A
-#define MSR_PP0_PERF_STATUS                     0x63B
+#define MSR_PP0_POWER_LIMIT            0x638
+#define MSR_PP0_ENERGY_STATUS          0x639
+#define MSR_PP0_POLICY                 0x63A
+#define MSR_PP0_PERF_STATUS            0x63B
 
 /* PP1 RAPL Domain, may reflect to uncore devices */
-#define MSR_PP1_POWER_LIMIT                     0x640
-#define MSR_PP1_ENERGY_STATUS					0x641
-#define MSR_PP1_POLICY                          0x642
+#define MSR_PP1_POWER_LIMIT            0x640
+#define MSR_PP1_ENERGY_STATUS          0x641
+#define MSR_PP1_POLICY                 0x642
 
 /* DRAM RAPL Domain */
-#define MSR_DRAM_POWER_LIMIT            0x618
-#define MSR_DRAM_ENERGY_STATUS          0x619
-#define MSR_DRAM_PERF_STATUS            0x61B
-#define MSR_DRAM_POWER_INFO                     0x61C
+#define MSR_DRAM_POWER_LIMIT           0x618
+#define MSR_DRAM_ENERGY_STATUS         0x619
+#define MSR_DRAM_PERF_STATUS           0x61B
+#define MSR_DRAM_POWER_INFO            0x61C
 
 /* RAPL UNIT BITMASK */
-#define POWER_UNIT_OFFSET                       0
-#define POWER_UNIT_MASK                         0x0F
+#define POWER_UNIT_OFFSET              0
+#define POWER_UNIT_MASK                0x0F
 
-#define ENERGY_UNIT_OFFSET                      0x08
-#define ENERGY_UNIT_MASK                        0x1F00
+#define ENERGY_UNIT_OFFSET             0x08
+#define ENERGY_UNIT_MASK               0x1F00
 
-#define TIME_UNIT_OFFSET                        0x10
-#define TIME_UNIT_MASK                          0xF000
+#define TIME_UNIT_OFFSET               0x10
+#define TIME_UNIT_MASK                 0xF000
 
-#define SIGNATURE_MASK   0xFFFF0
-#define IVYBRIDGE_E      0x306F0
-#define SANDYBRIDGE_E    0x206D0
+#define SIGNATURE_MASK                 0xFFFF0
+#define IVYBRIDGE_E                    0x306F0
+#define SANDYBRIDGE_E                  0x206D0
 
 
 Rapl::Rapl() {
